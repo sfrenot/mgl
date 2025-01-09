fb536d6b8875d839aec1f7dde4b4de72e37d02d6
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2025-01-09 13:53:24 +0100

    Ajout d'un script simple

---
+++ examples/simple.sh
@@ -0,0 +1,22 @@
+mkdir tmp
+cd tmp
+git init
+echo "A" > empty
+git add empty
+git commit -m'Ajout A dans empty/main'
+
+git branch feature
+git checkout feature
+
+echo "B" > empty
+git add empty
+git commit -m'Ajout B dans empty/feature'
+
+git checkout main
+echo "C" > empty
+git add empty
+git commit -m'Ajout C dans main'
+
+git checkout feature
+echo "Comment valider la modification ?"
+
